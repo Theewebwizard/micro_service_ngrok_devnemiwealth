a96c213f01d5f8e15a8aff6afec374099d8d6620
Pranshu <thewebwizard12@gmail.com>
2025-01-10 14:46:16 +0530

    port change

--- src/config/env.js
+++ src/config/env.js
@@ -10,6 +10,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   WHATSAPP_VERIFY_TOKEN: process.env.WHATSAPP_VERIFY_TOKEN,
   NGROK_AUTH_TOKEN: process.env.NGROK_AUTH_TOKEN,
-  CORS_ORIGINS: ['http://localhost:3000', 'https://localhost:3000/webhook/data'],
-  MAIN_BACKEND_URL: 'http://localhost:3000/webhook/data',
+  CORS_ORIGINS: ['http://localhost:5174', 'https://localhost:5174/webhook/data'],
+  MAIN_BACKEND_URL: 'http://localhost:5174/webhook/data',
 };
